f225bbeeb8b2c28bc135ad7c81941d0771f503f2
sophyaketch <sophyaketch@gmail.com>
2023-04-20 18:00:08 -0700

    variadic tasks

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,68 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char - it prints a char.
+ * @arg: list of arguments pointing to
+ *       character to be printed.
+ */
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
+}
+
+/**
+ * print_int - it prints an int.
+ * @arg: list of arguments pointing to
+ *       integer to be printed.
+ */
+void print_int(va_list arg)
+{
+	int num;
+
+	num = va_arg(arg, int);
+	printf("%d", num);
+}
+
+/**
+ * print_float - it prints  float.
+ * @arg: list of arguments pointing to
+ *        float to be printed.
+ */
+void print_float(va_list arg)
+{
+	float num;
+
+	num = va_arg(arg, double);
+	printf("%f", num);
+}
+
+/**
+ * print_string - it prints string.
+ * @arg: list of arguments pointing to
+ *       string to be printed.
+ */
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	printf("%s", str);
+}
